84f6a00b26715a3025fb8667b5e876870cddd4f4
Vincent Bourdaraud <vincent.bourdaraud@brimarx.com>
2015-08-20 08:25:28 +0200

    move main code to a main() function

--- server.js
+++ server.js
@@ -19,46 +19,8 @@
  # under the License.
  ##############################################################
  */
-opt = require('node-getopt').create([
-    ['p' , 'port=PORT'           , 'http server port; default=8080'],
-    ['l' , 'log-level=LOGLEVEL'  , 'log level (debug|info|warn|error); default=info'],
-    ['h' , 'help'                , 'display this help'],
-])
-.bindHelp()
-.parseSystem();
-if (!opt.options['log-level']) opt.options['log-level']='info';
-if (!opt.options.port) opt.options.port=8080;
-//console.info(opt);
 
 var lugg = require('lugg');
-lugg.init({level: opt.options['log-level']});
-var log = lugg('servicebox');
-
-log.debug('DEBUG enabled');
-log.info('INFO enabled');
-
-// Module dependencies.
-var application_root = __dirname,
-    express = require( 'express' ); //Web framework
-
-//Create server
-var app = express();
-
-// Configure server
-app.set('case sensitive routing', true);
-app.configure( function() {
-    //parses request body and populates request.body
-    app.use( express.bodyParser() );
-
-    //checks request.body for HTTP method overrides
-    app.use( express.methodOverride() );
-
-    //perform route lookup based on url and HTTP method
-    app.use( app.router );
-
-    //Show all errors in development
-    app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
 
 var echo = function(req, res) {
     lugg('echo').info('echoing %s', req.params.txt);
@@ -100,16 +62,65 @@ var env_hostname = function(req, res) {
     res.send(os.hostname());
 }
 
-var baseurl = '/api/v2';
-app.get(baseurl+'/echo/:txt', echo);
-app.get(baseurl+'/echooo/:txt/:delay', delayedEcho);
-app.get(baseurl+'/ECHO/:txt', intensiveEcho);
-app.get(baseurl+'/env/vars', env_vars)
-app.get(baseurl+'/env/vars/:name', env_var)
-app.get(baseurl+'/env/hostname', env_hostname)
+var main = function() {
+    // Read CLI args
+    opt = require('node-getopt').create([
+        ['p' , 'port=PORT'           , 'http server port; default=8080'],
+        ['l' , 'log-level=LOGLEVEL'  , 'log level (debug|info|warn|error); default=info'],
+        ['h' , 'help'                , 'display this help'],
+    ])
+    .bindHelp()
+    .parseSystem();
+    if (!opt.options['log-level']) opt.options['log-level']='info';
+    if (!opt.options.port) opt.options.port=8080;
+    //console.info(opt);
+    
+    // Init logs
+    lugg.init({level: opt.options['log-level']});
+    var log = lugg('servicebox');
 
-//Start server
-var port = opt.options.port;
-app.listen( port, function() {
-    log.warn('Express server listening on port %d in %s mode', port, app.settings.env);
-});
+    log.debug('DEBUG enabled');
+    log.info('INFO enabled');
+
+    // Module dependencies.
+    var application_root = __dirname,
+        express = require( 'express' ); //Web framework
+
+    //Create server
+    var app = express();
+
+    // Configure server
+    app.set('case sensitive routing', true);
+    app.configure( function() {
+        //parses request body and populates request.body
+        app.use( express.bodyParser() );
+
+        //checks request.body for HTTP method overrides
+        app.use( express.methodOverride() );
+
+        //perform route lookup based on url and HTTP method
+        app.use( app.router );
+
+        //Show all errors in development
+        app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+    });
+    
+    // Bind REST resources
+    var baseurl = '/api/v2';
+    app.get(baseurl+'/echo/:txt', echo);
+    app.get(baseurl+'/echooo/:txt/:delay', delayedEcho);
+    app.get(baseurl+'/ECHO/:txt', intensiveEcho);
+    app.get(baseurl+'/env/vars', env_vars)
+    app.get(baseurl+'/env/vars/:name', env_var)
+    app.get(baseurl+'/env/hostname', env_hostname)
+
+    //Start server
+    var port = opt.options.port;
+    app.listen( port, function() {
+        log.warn('Express server listening on port %d in %s mode', port, app.settings.env);
+    });
+}
+
+if(require.main === module) {
+  main();
+}
