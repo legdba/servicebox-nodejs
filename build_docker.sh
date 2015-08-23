8aeae2fcf3f841a247a8320bf1a89bad77ac7b12
Vincent Bourdaraud <vincent.bourdaraud@brimarx.com>
2015-08-24 00:02:11 +0200

    preparing for auto-build of dockers

---
+++ build_docker.sh
@@ -0,0 +1,304 @@
+#!/bin/bash
+###############################################################################
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+###############################################################################
+# Build Docker from a tgz archive of the application and a
+# Dockerfile template in docker/Dockerfile.in file.
+#
+# This script MUST be run from the git repo of the application.
+#
+# Requirements for the archive:
+# - TODO
+#
+# Requirements for the Dockerfile.in
+# - TODO
+#
+# ENV requirements:
+# - TODO
+#
+# In addition to that the script auto-detect the application
+# name version and branch based on git commands. The name is
+# the repo name, the version is a mix of revision and hash,
+# branch is the branch name with master set to an empty name.
+#
+###############################################################################
+
+# Ensure that when using pipes $? is non-zero if any of the pipe commands fail
+# in order to allow || exit 1 to catch it and exit.
+set -o pipefail
+
+###############################################################################
+# Global variables
+###############################################################################
+LOCAL=NO
+DRYRUN=NO
+
+###############################################################################
+# Functions
+###############################################################################
+
+# Print and run
+function exe() {
+    echo "\$ $@"
+    if [ ${DRYRUN} == NO ]
+    then
+        eval "$@" || exit 2
+    fi
+}
+
+# run without printing (convenient for commands with secrets)
+function run() {
+    if [ ${DRYRUN} == NO ]
+    then
+        eval "$@" || exit 2
+    fi
+}
+
+# Build archive for NPM application (NodeJS)
+function build_archive_via_npm() {
+    echo "detected NPM build"
+    NAME=$1
+    DIR=$2
+    exe "./build_package.sh --type=npm --app-name=${NAME} --pkg-dir=${DIR}"
+}
+
+# Build archive via Gradle as a fat jar
+function build_archive_via_gradle_fatjar() {
+    echo "detected GRADLE build"
+    NAME=$1
+    DIR=$2
+    exe "./build_package.sh --type=gradle-fatjar --app-name=${NAME} --pkg-dir=${DIR}"
+}
+
+# Help
+function print_help() {
+    echo -e "\
+usage: [options]\n\
+Options:\n\
+\n\
+  --local               Do not push docker images to docker registry.\n\
+\n\
+  --dry                 Dry run; commands are echoed but not run.\n\
+\n\
+  -h,--help             Display this help.\n\
+"
+}
+
+
+###############################################################################
+# Main
+###############################################################################
+
+###############################################################################
+# Parse arguments
+for i in "$@"
+do
+case $i in
+    -h|--help)
+        print_help
+        exit 0
+    ;;
+    --local)
+        LOCAL=YES
+        shift # past argument with no value
+    ;;
+    --dryrun)
+        DRYRUN=YES
+        shift # past argument with no value
+    ;;
+    -*)
+        echo "invalid option ${i#*=}" >&2
+        exit 1
+    ;;
+    *)
+        # ignore, arguments, they are processed later (see below)
+    ;;
+esac
+done
+
+if [[ -n $1 ]]; then
+    echo "too many arguments" >&2
+    exit 1
+fi
+
+echo
+echo "=== read arguments ==="
+echo "LOCAL           = ${LOCAL}"
+echo "DRYRUN          = ${DRYRUN}"
+
+###############################################################################
+# Handle dry-run mode
+if [ $DRYRUN == YES ]
+then
+    echo
+    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "+ DRY RUN MODE ENABLED; commands will be printed but NOT applied +"
+    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo
+fi
+
+###############################################################################
+# Check required variable
+echo
+echo "=== checking environment ==="
+
+# Check the env contains the Docker repo to push Docker image to
+# use "quai.io" for quay.io
+# use "hub.docker.com" for default docker hub
+if [ -z "${DOCKER_REPO}" ]
+then
+    echo "please set DOCKER_REPO"
+    exit 1
+fi
+
+# Check the env contains the Docker repo login to push Docker image to
+if [ -z "${DOCKER_REPO_USER}" ]
+then
+    echo "please set DOCKER_REPO_USER"
+    exit 1
+fi
+
+# Check the env contains the Docker repo token to push Docker image to
+if [ -z "${DOCKER_REPO_TOKEN}" ]
+then
+    echo "please set DOCKER_REPO_TOKEN"
+    exit 1
+fi
+
+# Check the env contains the Docker repo user email to push Docker image to
+if [ -z "${DOCKER_REPO_EMAIL}" ]
+then
+    echo "please set DOCKER_REPO_EMAIL"
+    exit 1
+fi
+
+# Check the env contains artifact path; auto set by CircleCI, Travis and so on
+if [ -z "${ARTIFACTS_PATH}" ]
+then
+    echo "please set ARTIFACTS_PATH"
+    exit 1
+fi
+
+###############################################################################
+# Set all variables
+echo
+echo "=== collecting facts ==="
+
+APP_REVISION=$(git rev-list --count HEAD)
+APP_HASH=$(git rev-parse --short HEAD)
+APP_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "${APP_BRANCH}" == "master" ]; then
+    APP_BRANCH=""
+else
+    APP_BRANCH="-${APP_BRANCH}"
+fi
+APP_NAME=$(basename `git rev-parse --show-toplevel`)
+APP_VERSION="r${APP_REVISION}-${APP_HASH}${APP_BRANCH}"
+APP_FULLNAME="${APP_NAME}-${APP_VERSION}"
+ARTIFACT="${APP_FULLNAME}"
+
+echo
+echo "APP_NAME       = ${APP_NAME}"
+echo "APP_VERSION    = ${APP_VERSION}"
+echo "APP_FULLNAME   = ${APP_FULLNAME}"
+echo "APP_BRANCH     = ${APP_BRANCH}"
+echo "BRANCH         = ${BRANCH}"
+echo "VERSION        = ${VERSION}"
+echo "ARTIFACT       = ${ARTIFACT}"
+echo "ARTIFACTS_PATH = ${ARTIFACTS_PATH}"
+
+###############################################################################
+# Generate archive
+echo
+echo "=== generating archive ==="
+if [ -f ./package.json ]; then
+    build_archive_via_npm ${ARTIFACT} ${ARTIFACTS_PATH}
+elif [ -f ./gradlew ]; then
+    build_archive_via_gradle_fatjar ${ARTIFACT} ${ARTIFACTS_PATH}
+else
+    echo "failed to detect build framework" >&2
+    exit 2
+fi
+
+# Check archive has been properly generated
+if [ $DRYRUN == NO ]; then
+    if [ ! -f ${ARTIFACTS_PATH}/${ARTIFACT}.tgz ]
+    then
+        echo file not found: ${ARTIFACTS_PATH}/${ARTIFACT}.tgz
+        exit 1
+    fi
+    if [ ! -f ${ARTIFACTS_PATH}/${ARTIFACT}.tgz.sha1 ]
+    then
+        echo file not found: ${ARTIFACTS_PATH}/${ARTIFACT}.tgz.sha1
+        exit 1
+    fi
+fi
+
+###############################################################################
+# Generate Dockerfile
+echo
+echo "=== Generating Dockerfile and docker resources archive ==="
+exe "cat ./docker/Dockerfile.in \
+     | sed \"s/__APP_FULLNAME__/$(echo ${APP_FULLNAME} | sed -e 's/[\/&]/\\&/g')/g\" \
+     | sed \"s/__APP_NAME__/$(echo ${APP_NAME} | sed -e 's/[\/&]/\\&/g')/g\" \
+     | sed \"s/__APP_VERSION__/$(echo ${APP_VERSION} | sed -e 's/[\/&]/\\&/g')/g\" \
+     | sed \"s/__ARTIFACT__/$(echo ${ARTIFACT} | sed -e 's/[\/&]/\\&/g')/g\" \
+     > ${ARTIFACTS_PATH}/Dockerfile"
+exe "cp ./docker/* ${ARTIFACTS_PATH}/"
+
+###############################################################################
+# Generate Docker Image
+echo
+echo "=== generating Docker Image(s) ==="
+exe "cd ${ARTIFACTS_PATH}"
+if [ $LOCAL == NO ]
+then
+    echo "\$ docker login --username=${DOCKER_REPO_USER} --password=\${DOCKER_REPO_TOKEN} --email=${DOCKER_REPO_EMAIL} ${DOCKER_REPO}"
+    run "docker login --username=${DOCKER_REPO_USER} --password=${DOCKER_REPO_TOKEN} --email=${DOCKER_REPO_EMAIL} ${DOCKER_REPO}"
+fi
+DOCKER_IMAGE_NAME="${DOCKER_REPO}/legdba/${APP_NAME}"
+exe "docker build --pull=true --tag=\"${DOCKER_IMAGE_NAME}\" ."
+exe "docker run -ti -P ${DOCKER_IMAGE_NAME} --help"
+exe "cd -"
+
+###############################################################################
+# Push Docker images
+echo
+if [ $LOCAL == NO ]
+then
+
+    echo "=== pushing Docker Image(s) ==="
+    
+    # Push an image with a label set to the binary version
+    DOCKER_IMAGE_LABEL="${DOCKER_IMAGE_NAME}:${APP_VERSION}"
+    exe "docker tag -f ${DOCKER_IMAGE_NAME} ${DOCKER_IMAGE_LABEL}"
+    exe "docker push ${DOCKER_IMAGE_LABEL}"
+    
+    if [ "${APP_BRANCH}" == "master" ]
+    then
+        # If we are on master push and image with label 'latest'
+        DOCKER_IMAGE_LABEL="${DOCKER_IMAGE_NAME}:latest"
+        exe "docker tag -f ${DOCKER_IMAGE_NAME} ${DOCKER_IMAGE_LABEL}"
+        exe "docker push ${DOCKER_IMAGE_LABEL}"
+    else
+        # If we are NOT master push and image with label set to the branch name
+        DOCKER_IMAGE_LABEL="${DOCKER_IMAGE_NAME}:${APP_BRANCH}"
+        exe "docker tag -f ${DOCKER_IMAGE_NAME} ${DOCKER_IMAGE_LABEL}"
+        exe "docker push ${DOCKER_IMAGE_LABEL}"
+    fi
+fi
---
+++ build_package.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+##############################################################
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+##############################################################
+# Convert an application to a standardized package ready for
+# install
+##############################################################
+
+# Ensure that when using pipes $? is non-zero if any of the pipe commands fail
+# in order to allow || exit 1 to catch it and exit.
+set -o pipefail
+
+TYPE=""
+APP_NAME=""
+PKG_DIR=""
+
+function print_help() {
+    echo -e "\
+usage: [options] --type=(npm|gradle-farjar) --app-name=NAME --pkg-dir=DIR\n\
+\n\
+Mandatory Parameters:\n\
+  --type=TYPE           Build manager type, used to select the packaging\n\
+                        mechanism; shall be any of (npm|gradle-farjar)\n\
+\n\
+                        TYPE='npm'\n\
+                        Packaging a npm application to a '{APP_NAME}.tgz'\n\
+                        archive with all the application source code.\n\
+\n\
+                        REQUIREMENTS:\n\
+                        - current dir must be the package.json dir\n\
+                        - application source must be in a subdir of the current\n\
+                          directory, a single subdir\n\
+                        - application source dir must be the basedir of the\n\
+                          package.json 'main' property\n\
+\n\
+                        TYPE='gradle-farjar'\n\
+                        Packaging a gradle fatJar application to a\n\
+                        '{APP_NAME}.tgz' which ocntains only the fat jar\n\
+                        renamed to '{APP_NAME}.jar' and without sub dir.
+\n\
+                        REQUIREMENTS:\n\
+                        - current dir must be the gradlew script dir\n\
+                        - current dir must be the build.gradle script dir\n\
+                        - 'gradlew fatJAr' command must build the fat jar and\n\
+                           outputs a 'fatjar: {filename}' line\n\
+                        - the fat jar must be generated in ./build/libs (this\n\
+                          is the default in gradle)\n\
+\n\
+  --app-name=APP_NAME   Name used for package generation; exact package name.\n\
+\n\
+  --pkg-dir=PKG_DIR     Directory where to generate the package.\n\
+\n\
+Optional Parameters:\n\
+  -h,--help             Display this help.\n\
+"
+}
+
+function package_npm() {
+    # Get the dir with the application root
+    APP_DIR=$(dirname $(cat package.json | egrep 'main" *:' | awk -F ":" '{ print $2 }' | awk -F '"' '{ print $2 }'))
+    if [ -z $APP_DIR ]; then
+        echo "cannot find source directory from package.json 'main' property"
+        exit 2
+    fi
+    if [ ! -d $APP_DIR ]; then
+        echo "directory not found: ${APP_DIR}"
+        exit 2
+    fi
+
+    # Get license file if any
+    if [ -f ./LICENSE ]; then
+        LICENSE="LICENSE"
+    else
+        LICENSE=""
+    fi
+
+    # Archive and sha1
+    PKG="${PKG_DIR}/${APP_NAME}.tgz"
+    SHA="${PKG}.sha1"
+    tar czf ${PKG} package.json ${LICENSE} ${APP_DIR}/* || exit 3
+    sha1sum ${PKG} > ${SHA}
+
+    echo "package : ${PKG}"
+    echo "hash    : ${SHA}"
+}
+
+function package_gradle_fatjar() {
+    # Build fat jar and get it's name
+    FN=$(./gradlew fatJar | sed -n "s/ *fatjar *: *\(.*\)/\1/p")
+    if [ $? -ne 0 ]; then
+        exit 2
+    fi
+
+    JAR="${APP_NAME}.jar"
+    PKG="${APP_NAME}.tgz"
+    SHA="${PKG}.sha1"
+
+    # Rename, archive and sha1
+    cp ./build/libs/${FN} ${JAR} || exit 3
+    cd ${PKGDIR} || exit 3
+    tar cvf ${PKG} ${JAR} || exit 3
+    rm ${JAR} || exit 3
+    sha1sum ${PKG} > ${SHA} || exit 3
+    cd -
+
+    echo "package : ${PKG_DIR}/${PKG}"
+    echo "hash    : ${PKG_DIR}/${SHA}"
+}
+
+# Parse arguments
+for i in "$@"
+do
+case $i in
+    -h|--help)
+        print_help
+        exit 0
+    ;;
+    --type=*)
+        TYPE="${i#*=}"
+        shift # past argument
+    ;;
+    --app-name=*)
+        APP_NAME="${i#*=}"
+        shift # past argument
+    ;;
+    --pkg-dir=*)
+       PKG_DIR="${i#*=}"
+        shift # past argument
+    ;;
+    *)
+        echo "invalid argument: $i" >&2
+        exit 1
+    ;;
+esac
+done
+if [[ -z $TYPE ]] || [[ -z $APP_NAME ]] || [[ -z $PKG_DIR ]]; then
+    MISSING=""
+    if [[ -z $TYPE ]]; then
+        MISSING="$MISSING --type"
+    fi
+    if [[ -z $APP_NAME ]]; then
+        MISSING="$MISSING --app-name"
+    fi
+    if [[ -z $PKG_DIR ]]; then
+        MISSING="$MISSING --pkg-dir"
+    fi
+    echo "missing required argument(s):${MISSING}" >&2
+    exit 1
+fi
+
+#echo "TYPE            = ${TYPE}"
+#echo "APP_NAME        = ${APP_NAME}"
+#echo "PKG_DIR         = ${PKG_DIR}"
+
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:" >&2
+    tail -1 $1
+fi
+
+case $TYPE in
+    npm)
+        package_npm
+    ;;
+    gradle-fatjar)
+        package_gradle_fatjar
+    ;;
+    *)
+        echo "invalid type $TYPE" >&2
+        exit 1
+    ;;
+esac
