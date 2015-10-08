408443f2ed55971a5f0a7d07e097e3812e22e121
Vincent Bourdaraud <vincent.bourdaraud@brimarx.com>
2015-10-08 08:01:57 +0200

    add redis cluster support

---
+++ app/api/helpers/rediscluster_backend.js
@@ -0,0 +1,105 @@
+/**
+ ##############################################################
+ # Licensed to the Apache Software Foundation (ASF) under one
+ # or more contributor license agreements.  See the NOTICE file
+ # distributed with this work for additional information
+ # regarding copyright ownership.  The ASF licenses this file
+ # to you under the Apache License, Version 2.0 (the
+ # "License"); you may not use this file except in compliance
+ # with the License.  You may obtain a copy of the License at
+ #
+ #   http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing,
+ # software distributed under the License is distributed on an
+ # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ # KIND, either express or implied.  See the License for the
+ # specific language governing permissions and limitations
+ # under the License.
+ ##############################################################
+ */
+'use strict';
+
+var makeClass = require('./make_class');
+var util = require('util');
+var lugg = require('lugg');
+var log = lugg('rediscluster-clt');
+var Redis = require('ioredis');
+
+var RedisClusterBackend = makeClass.makeClass();
+exports.RedisClusterBackend = RedisClusterBackend;
+
+RedisClusterBackend.prototype.constructor = function() {};
+
+/**
+ * Connect Redis cluster backend.
+ * @param callback Executes callback(err) with err set upon failure, with err set to null upon success.
+ * The RedisClusterBackend is ready to be used as soon as the calllback has been called with a success.
+ */
+RedisClusterBackend.prototype.init = function init(jsoncfg, callback) {
+    var self = this;
+    var nodes = [];
+    jsoncfg.contactPoints.forEach(function(entry) {
+        var m = entry.match(/([^\:]*)(?::(.*))?/);
+        var host=m[1];
+        var port=m[2]||6379;
+        nodes.push({host:host, port:port});
+    });
+    
+    // NOTE regarding ioredis behavior:
+    // By default ioredis will try to connect to the cluster forever, suspending any ongoing request
+    // till the cluster is back online. This is annoying since it cause huge delay for the client without
+    // any wait to control it.
+    // The implementation below is forcing enableOfflineQueue:false. This means that if the cluster is offline
+    // any request will fail immediatly without retry which sounds better.
+    // TODO: test if this cause errors upon a single master node failure.
+    self.cluster = new Redis.Cluster(nodes, {enableOfflineQueue:false});
+    
+    var redisInitErrorCallback = function redisInitCallback(err) {
+        callback(err);
+    };
+    
+    self.cluster
+    .once('error', redisInitErrorCallback)
+    .once('ready', function redisInitReadyCallback(stream) {
+        // Remove existing event handler and setup new ones to logs cluster state changes
+        // (we don't want the .once('error',...) event handler below to do a callback later on upon error
+        //  while we have already setup the backend connection once and did a callback)
+        self.cluster.removeListener('error', redisInitErrorCallback);
+        self.cluster.on('error', function redisErrorCallback(err) {
+            log.warn('redis-cluster error: %s', err);
+        }).on('connect', function redisConnectCallback(stream) {
+            log.warn('redis-cluster connected: %s', stream);
+        }).on('reconnecting', function redisReconnectingCallback() {
+            log.warn('redis-cluster reconnecting');
+        }).on('end', function redisEndCallback() {
+            log.warn('redis-cluster ended');
+        }).on('drain', function redisDrainCallback() {
+            log.warn('redis-cluster drained');
+        }).on('idle', function redisIdleCallback() {
+            log.warn('redis-cluster idle: %s');
+        });
+        // Test cluster
+        log.info('testing backend with a sum(\'0\', 0) request...')
+        self.addAndGet('0', 0, function incrCallback(err, result) {
+            if (err) callback(err);
+            log.info("counter: " + result);
+            log.info('backend test passed');
+            callback(null); // cluster is up and running, init() is done
+        });
+    });
+};
+
+/**
+ * Add 'number' to counter 'id' and return the new value in callback.
+ * @param callback Executes callback(err, new_counter) with err set upon error and with new_counter upon success
+ */
+RedisClusterBackend.prototype.addAndGet = function addAndGet(id, number, callback) {
+    this.cluster.incrby('servicebox:calc:sum:'+id, number, function incrCallback(err, result) {
+        if (err) {
+            if (callback) callback(err);
+        } else {
+            if (callback) callback(null, result);
+        }
+    });
+};
